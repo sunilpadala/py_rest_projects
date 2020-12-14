5f45c883b930a264826a5acc65e682b40e5018b5
Sunil PC <sunil4frnz@gmail.com>
2020-12-14 22:59:39 +0530

    Trvel app package

---
+++ Travelapp/Test_Travel_assistant.py
@@ -0,0 +1,25 @@
+import unittest
+import Travel_assistant
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        t = Travel_assistant.travel_assistant("India","Canada")
+        self.src = t.source_details()
+        self.dst = t.dest_details()
+
+    def test_src_name(self):
+        self.assertEqual(self.src[0]['name'], "India")
+
+    def test_src_capital(self):
+        self.assertEqual(self.src[0]['capital'], "New Delhi")
+
+    def test_dest_name(self):
+        self.assertEqual(self.dst[0]['name'], "Canada")
+
+    def test_dest_a3code(self):
+        self.assertEqual(self.src[0]['alpha3Code'], "CAN")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Travelapp/Travel_assistant.py
@@ -0,0 +1,46 @@
+import requests
+import money_converter
+import pprint
+
+class travel():
+    def country_deets(self,ctry):
+        base_path="https://restcountries.eu/rest/v2"
+        uri="/name/"
+        srch_ctry=ctry
+        query="fullText=true"
+        r=requests.get(base_path+uri+srch_ctry+"?"+query)
+
+        if r.status_code in range(200,300):
+            #print("search success")
+            #pprint.pprint(r.json())
+            return r.json()
+        else:
+            print("API server GET fail")
+
+
+class assistant():
+    def say_hi(self):
+        print("Hello Folks, I'm happy to help you" )
+
+class travel_assistant(travel,assistant):
+
+    def __init__(self,source,dest):
+        self.source=source
+        self.dest=dest
+
+    def dest_details(self):
+        return self.country_deets(self.dest)
+
+    def source_details(self):
+        return self.country_deets(self.source)
+
+    def comp_src_to_dest(self):
+        src_deets = self.source_details()
+        dst_deets = self.dest_details()
+        #print(dst_deets,"\n",src_deets)
+        print(f"1. Book a flight between {src_deets[0]['capital']} and {dst_deets[0]['capital']}")
+        print(f"2. Once you reach there, you'll meet {dst_deets[0]['demonym']} immigration")
+
+if __name__ == '__main__':
+    t=travel_assistant("India","Canada")
+    t.comp_src_to_dest()
